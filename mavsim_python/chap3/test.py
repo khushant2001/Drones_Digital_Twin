00a96a5829588f16fb73d3ad621825d97db2ec8b
khushant2001 <khushant.khurana@cooper.edu>
2022-10-04 00:04:42 -0400

    test.py_v2

--- mavsim_python/chap3/test.py
+++ mavsim_python/chap3/test.py
@@ -31,18 +31,18 @@ z_yaw=[]
 mass = .5
 radius = .13
 initial = np.array([
-    [0],
-    [0],
-    [-100],
-    [0],
-    [0],
-    [0],
-    [0],
-    [0],
-    [0],
-    [0],
-    [5],
-    [5],
+    [0.],
+    [0.],
+    [-100.],
+    [0.],
+    [0.],
+    [0.],
+    [0.],
+    [0.],
+    [0.],
+    [0.],
+    [0.],
+    [5.],
     [0]
     ])
 class MavDynamics:
@@ -84,15 +84,15 @@ class MavDynamics:
         x.append(self._state[0])
         y.append(self._state[1])
         z.append(self._state[2])
-        x_vel.append(self._state[3])
-        y_vel.append(self._state[4])
-        z_vel.append(self._state[5])
-        x_roll.append(self._state[10])
-        y_pitch.append(self._state[11])
-        z_yaw.append(self._state[12])
+        x_vel.append(self._state.item(3))
+        y_vel.append(self._state.item(4))
+        z_vel.append(self._state.item(5))
+        x_roll.append(self._state.item(10))
+        y_pitch.append(self._state.item(11))
+        z_yaw.append(self._state.item(12))
         # update the message class for the true state
         #self._update_true_state()
-
+        
     ###################################
     # private functions
     def _derivatives(self, state, forces_moments):
@@ -128,28 +128,39 @@ class MavDynamics:
             [2*(e1*e2+e3*e0), e2**2+e0**2-e1**2-e3**2, 2*(e2*e3-e1*e0)],
             [2*(e1*e3-e2*e0), 2*(e2*e3+e1*e0), e3**2+e0**2-e1**2-e2**2]
         ])@np.array([[u],[v],[w]])
-        north_dot = pos_dot[0]
-        east_dot = pos_dot[1]
-        down_dot = pos_dot[2]
-
+        """
+        pos_dot = np.array([
+            [e1,e2, e3],
+            [e3,e2,e1],
+            [e1,e2,e3]
+            ])
+        pos2 = np.array([[u],[v],[w]])
+        """
+        north_dot = pos_dot[0][0]
+        east_dot =pos_dot[1][0]
+        down_dot = pos_dot[2][0]
+        
         # position dynamics
+        
         vel = np.array([[r*v - q*w],
                           [p*w - r*u],
                           [q*u - p*v]]) + (1/mass)*np.array([[fx], [fy], [fz]])
-        u_dot = vel[0]
-        v_dot = vel[1]
-        w_dot = vel[2]
+        
+        
+        u_dot = vel[0][0]
+        v_dot = vel[1][0]
+        w_dot = vel[2][0]
         
         #Initializing the inertia matrix and required constants
         jx =  (2./5.)*mass*radius**2
         jy = jx
         jz = jx
-        jxy = 0
-        jyx = 0
-        jxz = 0
-        jzx = 0
-        jyz = 0
-        jzy = 0
+        jxy = 0.
+        jyx = 0.
+        jxz = 0.
+        jzx = 0.
+        jyz = 0.
+        jzy = 0.
         r0 = jx*jz-jxz**2
         r1 = (jxz*(jx-jy+jz))/r0
         r2 = (jz*(jz-jy)+jxz**2)/r0
@@ -160,39 +171,44 @@ class MavDynamics:
         r7 = ((jx-jy)*jx+jxz**2)/r0
         r8 = jx/r0
         # rotational kinematics
+        
         e_vel = .5*np.array([
-            [0,-p,-q,-r],
-            [p,0,r,-q],
-            [q,-r, 0, p],
-            [r,q,-p,0]
+            [0.,-p,-q,-r],
+            [p,0.,r,-q],
+            [q,-r, 0., p],
+            [r,q,-p,0.]
         ])@np.array([
             [e0],
             [e1],
             [e2],
             [e3]
         ])
-        e0_dot = e_vel[0]
-        e1_dot = e_vel[1]
-        e2_dot = e_vel[2]
-        e3_dot = e_vel[3]
+        e0_dot = e_vel[0][0]
+        e1_dot = e_vel[1][0]
+        e2_dot = e_vel[2][0]
+        e3_dot = e_vel[3][0]
 
         # rotatonal dynamics
+        """"
         w_dot = np.array([
                 [r1*p*q-r2*q*r],
                 [r5*p*r-r6*(p**2-r**2)],
                 [r7*p*q-r1*q*r]
-            ])+np.array([
+            ])
+        
+        w_2 = np.array([
                     [r3*l+r4*n],
                     [m/jy],
                     [r4*l+r8*n]
                 ])
-        p_dot = w_dot[0]
-        q_dot = w_dot[1]
-        r_dot = w_dot[2]
+        """
+        p_dot = r1*p*q-r2*q*r+r3*l+r4*n
+        q_dot = r5*p*r-r6*(p**2-r**2)+m/jy
+        r_dot = r7*p*q-r1*q*r+r4*l+r8*n
 
         # collect the derivative of the states
-        x_dot = np.array([[north_dot, east_dot, down_dot, u_dot, v_dot, w_dot,e0_dot, e1_dot, e2_dot, e3_dot, p_dot, q_dot, r_dot]]).T
-        return x_dot
+        x_dot = np.array([[north_dot, east_dot, down_dot, u_dot, v_dot, w_dot,e0_dot, e1_dot, e2_dot, e3_dot, p_dot, q_dot, r_dot]])
+        return x_dot.transpose()
     """
     def _update_true_state(self):
         # update the true state message:
@@ -218,12 +234,14 @@ forces_moments = np.array([
     ])
 for n in range(100):
     test.update(forces_moments)
-time = np.linspace(0,50,1000)
+time = np.linspace(0,100,100)
 
 plt.figure()
 plt.grid()
 plt.title("X_position")
 plt.plot(time,x)
+plt.xlim(0,100)
+plt.ylim(0,100)
 
 plt.figure()
 plt.grid()
