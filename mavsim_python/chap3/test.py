ae2a48044a810b0056afdce0f2db494e4686d265
khushant2001 <70731991+khushant2001@users.noreply.github.com>
2022-10-06 22:38:00 -0400

    Update test.py

--- mavsim_python/chap3/test.py
+++ mavsim_python/chap3/test.py
@@ -1,8 +1,9 @@
+
 """
 mav_dynamics
     - this file implements the dynamic equations of motion for MAV
     - use unit quaternion for the attitude state
-    
+   
 part of mavsimPy
     - Beard & McLain, PUP, 2012
     - Update history:  
@@ -33,7 +34,7 @@ z_yaw=[]
 mass = .145
 radius = .0365
 import math
-density =101 
+density =101
 cl = .22
 spin_factor = .233
 
@@ -44,31 +45,33 @@ class MavDynamics:
         # _state is the 13x1 internal state of the aircraft that is being propagated:
         # _state = [pn, pe, pd, u, v, w, e0, e1, e2, e3, p, q, r]
         self._state = initial = np.array([
-    [0.],#x
-    [0.],#y
-    [-100.],#z
-    [0.1],#x_dot
-    [0.1],#y_dot
-    [0.1],#z_dot
-    [0.1],#e0
-    [0.1],#e1
-    [0.1],#e2
-    [0.1],#e3
-    [0.1],#p
-    [10.],#q
-    [0.1]#r
+    [0.0001],#x
+    [.0001],#y
+    [0.0001],#z
+    [0.0001],#x_dot
+    [0.0001],#y_dot
+    [0.0001],#z_dot
+    [1.],#e0
+    [0.],#e1
+    [0.],#e2
+    [0.],#e3
+    [0.0001],#p
+    [0.0001],#q
+    [0.0001]#r
     ])
 
     ###################################
     # public functions
     def update(self):
         '''
-            Integrate the differential equations defining dynamics. 
+            Integrate the differential equations defining dynamics.
             Inputs are the forces and moments on the aircraft.
             Ts is the time step between function calls.
         '''
 
         # Integrate ODE using Runge-Kutta RK4 algorithm
+       
+       
         time_step = self.ts_simulation
         k1 = self._derivatives(self._state)
         k2 = self._derivatives(self._state + time_step/2.*k1)
@@ -83,22 +86,13 @@ class MavDynamics:
         e2 = self._state.item(8)
         e3 = self._state.item(9)
         normE = np.sqrt(e0**2+e1**2+e2**2+e3**2)
-        print(normE)
         self._state[6][0] = self._state.item(6)/normE
         self._state[7][0] = self._state.item(7)/normE
         self._state[8][0] = self._state.item(8)/normE
         self._state[9][0] = self._state.item(9)/normE
-        x.append(self._state[0])
-        y.append(self._state[1])
-        z.append(self._state[2])
-        x_vel.append(self._state.item(3))
-        y_vel.append(self._state.item(4))
-        z_vel.append(self._state.item(5))
-        x_roll.append(self._state.item(10))
-        y_pitch.append(self._state.item(11))
-        z_yaw.append(self._state.item(12))
+       
         # update the message class for the true state
-        #self._update_true_state()
+        self._update_true_state()
     ###################################
     # private functions
     def _derivatives(self, state):
@@ -119,30 +113,30 @@ class MavDynamics:
         p = state.item(10)
         q = state.item(11)
         r = state.item(12)
-
+       
         #   extract forces/moments
         vel_vector = np.array([[u, v, w]])
         v_unit = vel_vector/np.linalg.norm(vel_vector)
-        
+       
         omega = np.array([[p, q, r]])
         omega_unit = omega/np.linalg.norm(omega)
-        
+       
         new_vector = np.cross(omega_unit,v_unit)
         f_new = .5*cl*density*np.pi*radius**2*np.linalg.norm(vel_vector)*new_vector
-        fx = f_new[0][0]
+        fx = 0#f_new[0][0]
         fxx.append(fx)
         fy = 0
-        fz = mass*9.81 + f_new[0][2]
+        fz = 1.4#mass*9.81 #+ f_new[0][2]
         fzz.append(fz)
         l = 0
         m = 0
         n = 0
-        
+       
         # position kinematics
         pos_dot = np.array([
-            [e1**2+e0**2 - e2**2 - e3**2, 2*(e1*e2-e3*e0), 2*(e1*e3+e2*e0)],
-            [2*(e1*e2+e3*e0), e2**2+e0**2-e1**2-e3**2, 2*(e2*e3-e1*e0)],
-            [2*(e1*e3-e2*e0), 2*(e2*e3+e1*e0), e3**2+e0**2-e1**2-e2**2]
+            [(e1**2)+(e0**2) - (e2**2) - (e3**2), 2*((e1*e2)-(e3*e0)), 2*((e1*e3)+(e2*e0))],
+            [2*((e1*e2)+(e3*e0)), (e2**2)+(e0**2)-(e1**2)-(e3**2), 2*((e2*e3)-(e1*e0))],
+            [2*((e1*e3)-(e2*e0)), 2*((e2*e3)+(e1*e0)), (e3**2)+(e0**2)-(e1**2)-(e2**2)]
         ])@np.array([[u],[v],[w]])
         """
         pos_dot = np.array([
@@ -153,25 +147,30 @@ class MavDynamics:
         pos2 = np.array([[u],[v],[w]])
         """
         north_dot = pos_dot[0][0]
-        east_dot =pos_dot[1][0]
+        east_dot = pos_dot[1][0]
         down_dot = pos_dot[2][0]
-        print(north_dot)
+       
+       
+       
         # position dynamics
-        
-        vel = np.array([[r*v - q*w],
-                          [p*w - r*u],
-                          [q*u - p*v]]) + (1/mass)*np.array([[fx], [fy], [fz]])
-        
-        
+       
+        vel = np.array([
+                          [(r*v) - (q*w)],
+                          [(p*w) - (r*u)],
+                          [(q*u) - (p*v)]
+                          ]) + (1/mass)*np.array([[fx], [fy], [fz]])
+       
+       
         u_dot = vel[0][0]
         v_dot = vel[1][0]
-        w_dot = vel[2][0]
+        w_dot = vel[2][0]#WRONG!!!!
         
-        
+       
+       
         #Initializing the inertia matrix and required constants
         jx =  (2./5.)*mass*radius**2
-        jy = jx
-        jz = jx
+        jy = (2./5.)*mass*radius**2
+        jz = (2./5.)*mass*radius**2
         jxy = 0.
         jyx = 0.
         jxz = 0.
@@ -188,7 +187,7 @@ class MavDynamics:
         r7 = ((jx-jy)*jx+jxz**2)/r0
         r8 = jx/r0
         # rotational kinematics
-        
+       
         e_vel = .5*np.array([
             [0.,-p,-q,-r],
             [p,0.,r,-q],
@@ -212,35 +211,34 @@ class MavDynamics:
                 [r5*p*r-r6*(p**2-r**2)],
                 [r7*p*q-r1*q*r]
             ])
-        
+       
         w_2 = np.array([
                     [r3*l+r4*n],
                     [m/jy],
                     [r4*l+r8*n]
                 ])
         """
-        p_dot = r1*p*q-r2*q*r+r3*l+r4*n
+        #CHECK!!!!!
+        p_dot = r1*p*q-r2*q*r+r3*l+r4*n #probably wrong WRONG
         q_dot = r5*p*r-r6*(p**2-r**2)+m/jy
         r_dot = r7*p*q-r1*q*r+r4*l+r8*n
 
         # collect the derivative of the states
         x_dot = np.array([[north_dot, east_dot, down_dot, u_dot, v_dot, w_dot,e0_dot, e1_dot, e2_dot, e3_dot, p_dot, q_dot, r_dot]])
         return x_dot.transpose()
-    """
+   
     def _update_true_state(self):
         # update the true state message:
-        phi, theta, psi = Quaternion2Euler(self._state[6:10])
-        self.true_state.north = self._state.item(0)
-        self.true_state.east = self._state.item(1)
-        self.true_state.altitude = -self._state.item(2)
-        self.true_state.phi = phi
-        self.true_state.theta = theta
-        self.true_state.psi = psi
-        self.true_state.p = self._state.item(10)
-        self.true_state.q = self._state.item(11)
-        self.true_state.r = self._state.item(12)
-        """
-test = MavDynamics(.1)
+        x.append(0+self._state[0])
+        y.append(0.-self._state[1])
+        z.append(100-self._state[2])
+        x_vel.append(self._state.item(3))
+        y_vel.append(self._state.item(4))
+        z_vel.append(self._state.item(5))
+        x_roll.append(self._state.item(10))
+        y_pitch.append(self._state.item(11))
+        z_yaw.append(self._state.item(12))
+test = MavDynamics(.01)
 """
 forces_moments = np.array([
     [],
@@ -251,9 +249,9 @@ forces_moments = np.array([
     [0]
     ])
 """
-for n in range(40):
+for n in range(4):
     test.update()
-time = np.linspace(0,40,40)
+time = np.linspace(0,4,4)
 
 plt.figure()
 plt.grid()
@@ -269,7 +267,7 @@ plt.figure()
 plt.title("Altitude")
 plt.grid()
 plt.plot(time,z)
-
+"""
 plt.figure()
 plt.title("X_velocity")
 plt.grid()
@@ -279,12 +277,13 @@ plt.figure()
 plt.title("Y_velocity")
 plt.plot(time,y_vel)
 plt.grid()
-
+"""
 plt.figure()
 plt.title("Z_velocity")
 plt.grid()
 plt.plot(time, z_vel)
 plt.figure()
+"""
 plt.title("Roll vel")
 plt.plot(time,x_roll)
 plt.grid()
@@ -302,3 +301,4 @@ plt.plot(time,z_yaw)
 plt.figure()
 plt.title("vqfvqvqeveqv")
 plt.plot(y,z)
+"""
