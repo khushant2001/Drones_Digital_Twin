2693d0a7c7fd2c413d384e97f71b92a3bb10a241
CrimsonKicks <kevin.luo@cooper.edu>
2022-10-26 13:43:47 -0400

    in class additions v2

--- mavsim_python/chap5/test.py
+++ mavsim_python/chap5/test.py
@@ -1,14 +1,23 @@
 from control.matlab import *
 import numpy as np
 import math
-import matplotlib.plyplot
-import khushant.bigbrain
+import matplotlib.plyplot as plt
+from numpy import linalg as LA
 from chap5.trim import compute_trim
 import chap5.model_coef as MOD
 
 alpha1 = MOD.a_phi1
 alpha2 = MOD.a_phi2
 
+A = MOD.A_lon
+B = MOD.B_lon
+C = np.array([])
+
+#Defining Transfer Function Inputs for Roll
 s = tf('s')
-G =
-step(G)
+d_phi = 1/s
+alpha = 1/s
+
+G0 = alpha2 / (s*(s + alpha1))
+G1 = G0 * (alpha + (d_phi / alpha2))
+y,t tep(G)
