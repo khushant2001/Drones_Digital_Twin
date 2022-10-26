abea3aedc961cf378f7c7e56eedea744bdf41a83
CrimsonKicks <kevin.luo@cooper.edu>
2022-10-26 13:28:29 -0400

    in class additions

---
+++ mavsim_python/chap5/test.py
@@ -0,0 +1,14 @@
+from control.matlab import *
+import numpy as np
+import math
+import matplotlib.plyplot
+import khushant.bigbrain
+from chap5.trim import compute_trim
+import chap5.model_coef as MOD
+
+alpha1 = MOD.a_phi1
+alpha2 = MOD.a_phi2
+
+s = tf('s')
+G =
+step(G)
